b1bf0d97279b4b3af9587679202d1c22b66d636d
Boglárka Madar <boglarka.madar@qubews.hu>
2024-04-03 09:26:38 +0200

    footer

---
+++ Wordpress/wordress/app/public/wp-content/themes/digitalis-dietetika/footer.php
@@ -0,0 +1,29 @@
+
+<footer>
+<div class="separator"></div>
+
+<div class="row footer-wrap">
+    <div class="col-sm-6 footer-logo-wrap">
+        <img class="footer-logo" src="http://wordress.local/wp-content/uploads/2024/03/footer-logo.png" alt="footer logo">
+    </div>
+    <div class="col-sm-6 footer-menu-wrap">
+        <nav>
+            <ul>
+                <li><a class="footer-text">FELHASZNÁLÁSI FELTÉTELEK</a> </li>
+                <li><a>ADATVÉDELMI NYILATKOZAT</a> </li>
+                <li><a>GYIK</a> </li>
+                <li><a>KAPCSOLAT</a> </li>
+             </ul>
+        </nav>
+    </div>
+   
+</div>
+    
+</footer>
+
+
+<style>
+    
+
+
+</style>
